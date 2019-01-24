44068a29d32cb7115ed036c443f27e4944f0eeac
Steven L. Anderson II <andersst@tamu.edu>
2019-01-24 09:37:20 -0600

    Updating SampleInfile info

--- R/SampleInfile.R
+++ R/SampleInfile.R
@@ -12,11 +12,16 @@
 #'
 #' We recommend have repeated Barcodes and Plot numbers if there are multirow plots (mrowplot>1) as
 #' the plotshpcreatre function accounts for this redundance within the function.
-#'
-#'  [,1] Barcode     A unique identifier for each plot (character)
-#'  [,2] Plot        The number of each plot (numeric)
-#'  [,3] Range       The range [also called row] number of each plot in the experimental grid (numeric)
-#'  [,4] Row         The row [also called column] number of each plot in the experimental grid (numeric)
+#'  [,1]  Loc         An abbreviation to identify the trial location and year (character)
+#'  [,2]  Test        An abbreviation to define the trial name  (character)
+#'  [,3]  Plot        The number of each plot (numeric)
+#'  [,4]  Barcode     A unique identifier for each plot (character)
+#'  [,5]  Stock       The seed source which is planted in each plot (character)
+#'  [,6]  Pedigree    Information defining the lineage of the seed source (character)
+#'  [,7]  Entry       An additional unique identifer commonly used to track consistent pedigrees from different seed stocks (numeric)
+#'  [,8]  Rep         The replicate number of each plot (numeric)
+#'  [,9]  Range       The range [also called row] number of each plot in the experimental grid (numeric)
+#'  [,10] Row         The row [also called column] number of each plot in the experimental grid (numeric)
 #' @note
 #' Only Range, Row, Plot, and Barcode variables are necessary for the the proper execution of plotshpcreate,
 #' but additional columns may be included in the dataframe.
